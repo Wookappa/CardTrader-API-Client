8d4b84f936db1ecfe3ffb36e8d394786dbecb6b0
Wookappa <gflorian96@gmail.com>
2025-05-12 19:48:53 +0200

    Add Token provider for dynamic token

---
+++ src/CardTraderApi.Client/TokenProvider.cs
@@ -0,0 +1,30 @@
+﻿namespace CardTraderApi.Client
+{
+	public class TokenProvider : ITokenProvider
+	{
+		private string _jwtToken;
+		private readonly object _lock = new();
+
+		public string JwtToken
+		{
+			get
+			{
+				lock (_lock)
+				{
+					return _jwtToken;
+				}
+			}
+		}
+
+		public void SetToken(string jwtToken)
+		{
+			if (string.IsNullOrWhiteSpace(jwtToken))
+				throw new ArgumentNullException(nameof(jwtToken));
+
+			lock (_lock)
+			{
+				_jwtToken = jwtToken;
+			}
+		}
+	}
+}
